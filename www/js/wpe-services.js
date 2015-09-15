1f31cafa2440ad2bee5f2cdaeba9558bdd13702e
Justin Harris <justin.harris@wpengine.com>
2015-09-15 13:00:09 -0500

    Adding plan usage tab

--- www/js/wpe-services.js
+++ www/js/wpe-services.js
@@ -2,10 +2,9 @@
 var servicesApp = angular.module('wpApp.services');
 
 servicesApp.service('InstallService', function($http) {
-  var baseUrl = 'http://mypreview.wpengine.com'
+  var baseUrl = 'http://mypreview.wpengine.com';
 
   this.getForAccount = function(accountName) {
-    // TODO:  Hard coded IPs...
     var installUrl = baseUrl + '/api/accounts/' + accountName + '/installs/?wpe_apikey=devkey';
 
     return $http.get(installUrl);
@@ -22,7 +21,7 @@ servicesApp.service('InstallService', function($http) {
 
     return $http.get(errorsUrl);
   };
-  
+
   this.getStatusFeed = function(url) {
   	return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
   };
@@ -38,4 +37,56 @@ servicesApp.service('InstallService', function($http) {
 
     return $http.post(backupsUrl);
   };
+});
+
+
+servicesApp.service('AccountService', function($http) {
+  //var baseUrl = 'http://mypreview.wpengine.com';
+  var baseUrl = 'http://10.10.69.217:3000';
+
+  this.getUsage = function(accountName) {
+    var usageUrl = baseUrl + '/api/accounts/' + accountName + '/installs/plan_usage?wpe_apikey=devkey';
+
+    return $http.get(usageUrl);
+  };
+
+  this.planForUsage = function(usage) {
+    var plans = {
+      personal: {
+        name: 'Personal',
+        installs: 1,
+        visits: '25,000',
+        storage: 10
+      },
+      professional: {
+        name: 'Professional',
+        installs: 10,
+        visits: '100,000',
+        storage: 20
+      },
+      business: {
+        name: 'Business',
+        installs: 25,
+        visits: '400,000',
+        storage: 30
+      },
+      premium: {
+        name: 'Premium',
+        installs: 150,
+        visits: '1,000,000+',
+        storage: '100-300'
+      }
+    };
+
+    var noPlan = {
+        name: 'No',
+        installs: 0,
+        visits: 0,
+        storage: 0
+    };
+
+    return plans[usage.current_billing_cycle.account.plan] || noPlan;
+  };
+
+
 });
